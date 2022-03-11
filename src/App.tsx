afdae536c91727d4a6ea54f7412f99d75f5af705
Kevin Koobs <k.koobs@outlook.com>
2022-03-11 09:39:23 +0100

    Initiated create-react-app with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// const url = new URLSearchParams(window.location.search);
+// const test = [];
+// for (const [key, value] of url) {
+//   test.push({ [key]: value })
+// }
+
+// alert(JSON.stringify(test));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
